804268feaae4d7b2652b0418681ec465ccefae3d
kravchuk21 <krauchukvlad@gmail.com>
2023-03-13 19:22:47 +0300

    feat: add 'pre' syntax highlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,18 @@
-module.exports = function(config) {
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (config) {
     config.addPassthroughCopy("src/styles/");
     config.addWatchTarget("src/styles/");
+    config.addPassthroughCopy("src/projects/**/*.(jpg)");
+
+    config.addPlugin(syntaxHighlight);
 
     return {
         dir: {
             input: "src",
             includes: "_includes",
             output: "_site",
-            layouts: 'layouts',
+            layouts: "layouts",
         },
         templateFormats: ["md", "njk", "html"],
         markdownTemplateEngine: "njk",
